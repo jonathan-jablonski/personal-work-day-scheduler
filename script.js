8693cd6d0f24589eee0d7fd51427f8298dbdb05c
JJ <jonathanjablonski94@gmail.com>
2021-03-12 14:00:25 -0600

    Only one timeblock saves its data, timeblocks do not change color depending on current time

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 let today = moment();
+$('#currentDay').text(today.format ('dddd, MMMM, Do, h:mm:ss a'));
 
+$('.saveBtn').on('click', function (){
+    let btn = $(this);
+    let textarea = btn.siblings('textarea');
+    let val = textarea.val();
+    let id = textarea.attr('id');
 
-$('#currentDay').text(today.format ('dddd, MMMM, Do'));
+    localStorage.setItem(id, val);
+});
+
+$('textarea').each(function() {
+    let currentHour = moment().hours();
+
+    let time = $(this).attr('id');
+
+    if(time < currentHour) {
+        $(this).addClass('past');
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+    }else if(time > currentHour) {
+        $(this).addClass('future');
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+    }else{
+        $(this).addClass('present');
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+    }
+});
+
+$("#9").val(localStorage.getItem('9'));
